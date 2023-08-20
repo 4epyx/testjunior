7d9a8808405856750f94feba26ddbd872c291b67
4epyx <maksimka.arbuzov48@gmail.com>
2023-08-21 00:37:07 +0300

    create refresh token model

---
+++ model/refreshtoken.go
@@ -0,0 +1,20 @@
+package model
+
+/*
+RefreshToken represents a refresh token data, stored in database
+
+	Id -
+	UserGuid - user GUID
+	Expiration - expiration time of the token in unix format
+	Token - Hash of the token
+*/
+type RefreshToken struct {
+	//Id is a token UUID
+	Id string `json:"id" bson:"_id"`
+	//UserGuid is a token GUID
+	UserGuid string `json:"user_guid" bson:"user_guid"`
+	//Expiration is a time of expiration in unix format
+	Expiration int64 `json:"exp" bson:"exp"`
+	//Token is a hash of the refresh token
+	Token string `json:"token" bson:"token"`
+}
