9b702d21fee5458a3480f82e17166d0fc22c201f
4epyx <maksimka.arbuzov48@gmail.com>
2023-08-24 08:21:21 +0300

    create handler for refreshing token

--- handler/token.go
+++ handler/token.go
@@ -56,3 +56,37 @@ func (h *TokenHandler) GetAccessAndRefreshTokens(w http.ResponseWriter, r *http.
 
 	w.Write(jsonResp)
 }
+
+func (h *TokenHandler) RefreshToken(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"status": "invalid request body"}`))
+		return
+	}
+
+	oldRefreshToken := r.Form.Get("refresh_token")
+	if oldRefreshToken == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"status": "requst body not contains refresh token"}`))
+		return
+	}
+
+	tokens := Tokens{}
+	var err error
+
+	tokens.AccessToken, tokens.RefreshToken, err = h.service.RefreshToken(r.Context(), oldRefreshToken)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"status": "error occured while refreshing token"}`))
+		return
+	}
+
+	jsonResp, err := json.Marshal(tokens)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"status": "error occured while marshaling JSON"}`))
+		return
+	}
+
+	w.Write(jsonResp)
+}
