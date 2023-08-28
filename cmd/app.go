a3bd57b96d984425d03cfe139a62de40028a9000
4epyx <maksimka.arbuzov48@gmail.com>
2023-08-28 08:00:20 +0300

    set up logger

--- cmd/app.go
+++ cmd/app.go
@@ -4,20 +4,23 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/4epyx/testtask/handler"
+	"github.com/4epyx/testtask/middleware"
 	"github.com/4epyx/testtask/repository/mongorepo"
 	"github.com/4epyx/testtask/service"
 	"github.com/4epyx/testtask/util"
 	"github.com/4epyx/testtask/util/database"
+	"github.com/rs/zerolog"
 )
 
 func main() {
 	ctx := context.Background()
 
 	// get required environment variables
-	envs, errs := util.ParseEnv("SERVER_PORT", "DB_URI", "DB_NAME", "ACCESS_TOKEN_TTL", "REFRESH_TOKEN_TTL", "ACCESS_TOKEN_SECRET", "REFRESH_TOKEN_SECRET")
+	envs, errs := util.ParseEnv("SERVER_PORT", "DB_URI", "LOG_FILE", "DB_NAME", "ACCESS_TOKEN_TTL", "REFRESH_TOKEN_TTL", "ACCESS_TOKEN_SECRET", "REFRESH_TOKEN_SECRET")
 	if len(errs) != 0 {
 		panic(fmt.Sprintf("%v", errs))
 	}
@@ -52,17 +55,24 @@ func main() {
 	s := service.NewTokenService(repo, accessTokenTTL, refreshTokenTTL, []byte(envs["ACCESS_TOKEN_SECRET"]), []byte(envs["ERFRESH_TOKEN_SECRET"]))
 	h := handler.NewTokenHandler(s)
 
-	mux := setupRoutes(h)
+	// Create and open file for logs
+	logFile, err := os.Create(envs["LOG_FILE"])
+	if err != nil {
+		panic(err)
+	}
+	logger := zerolog.New(logFile)
+	mux := setupRoutes(h, &logger)
 
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", envs["SERVER_PORT"]), mux); err != nil {
 		panic(err)
 	}
 }
 
-func setupRoutes(h *handler.TokenHandler) *http.ServeMux {
+func setupRoutes(h *handler.TokenHandler, logger *zerolog.Logger) *http.ServeMux {
+	logMiddleware := middleware.NewLogMiddleware(logger)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/token/generate", h.GetAccessAndRefreshTokens)
-	mux.HandleFunc("/token/refresh", h.RefreshToken)
+	mux.Handle("/token/generate", logMiddleware.Log(http.HandlerFunc(h.GetAccessAndRefreshTokens)))
+	mux.Handle("/token/refresh", logMiddleware.Log(http.HandlerFunc(h.RefreshToken)))
 
 	return mux
 }
